34075a4d6433b2f5a9234966cfc8c03e5c0dd3d0
Tina <tinasylviac@gmail.com>
2021-07-14 19:28:48 -0400

    Toggling between X and O

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,19 +3,27 @@ import Square from './Square';
 import "../App.css";
 
 const Board = ()=> { 
-    const [square, setSquare] = useState([Array(9).fill(null)]);
+
+    const [square, setSquare] = useState(Array(9).fill(null));
+    const [X, setX] = useState(true);
+
     const renderSquare =(i) => {
         return(
             <Square value ={square[i]} onClick = {()=> handleClick(i)}/>
         )
     }
-    const handleClick =(i)=> {
-        
-        const squares = square.slice();
-        squares[i] = 'X' ;
-        setSquare(squares)
-
+    const handleClick =(i)=> {           
+            const squares = square.slice();
+            if (squares[i] === null) {  
+            squares[i] = X ? 'X': 'O';
+            setSquare(squares);
+            setX(!X);
+        }
+        else{
+            alert(" Sorry! Cannot make this move.");
+        }
     }
+
     return (
         <div className="board">
             <div className="board-row">
